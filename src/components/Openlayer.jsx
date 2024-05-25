5b31bcfbc5c5ee7e2eba0209ad8844b3f5c26b96
OguzhanAcer <oguzhanturgut611@gmail.com>
2024-05-25 21:47:28 +0300

    freehand drawing

--- src/components/Openlayer.jsx
+++ src/components/Openlayer.jsx
@@ -2,40 +2,71 @@ import React, {useEffect, useRef} from 'react';
 import Map from 'ol/Map';
 import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
-import VectorSource from 'ol/source/Vector';
-import VectorLayer from 'ol/layer/Vector';
 import {OSM} from "ol/source";
 import * as olProj from "ol/proj";
-import "../App.css";
 import "ol/ol.css";
+import "../App.css";
+import {DragRotate, Draw} from "ol/interaction";
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
 
 function Openlayer() {
-    const vectorSource = new VectorSource();
-    const vectorLayer = new VectorLayer();
-
     const mapRef = useRef(null);
+    let map;
+
+    const source = new VectorSource({wrapX: false});
+
+    const vector = new VectorLayer({
+        source: source,
+    });
 
     useEffect(() => {
         olProj.useGeographic();
 
-        new Map({
+        map = new Map({
             view: new View({
                 center: [39.9392, 32.8962],
-                zoom: 2
+                zoom: 4,
+                maxZoom: 6,
+                minZoom: 1,
+                /*rotation: 0.25*/
             }),
             layers: [
                 new TileLayer({
                     source: new OSM({
                         crossOrigin: 'anonymous'
                     })
-                })
+                }), vector
             ],
-            target: mapRef.current
+            target: mapRef.current,
+            keyboardEventTarget: document
         });
+
+        map.on('click', function (e) {
+            const coordinates = e.coordinate;
+            console.log(coordinates);
+        });
+
+        const isAltKeyPressed = (event) => event.altKey;
+
+        const dragRotateInteraction = new DragRotate({
+            condition: isAltKeyPressed
+        });
+        map.addInteraction(dragRotateInteraction);
+
+        const drawInteraction = new Draw({
+            type: "Polygon",
+            freehand: true,
+            source: source
+        })
+        map.addInteraction(drawInteraction);
+
     }, []);
 
     return (
-        <div id="js-map" className="map" ref={mapRef} />
+        <>
+            <div id="js-map" className="map" ref={mapRef}/>
+        </>
     );
 }
 
